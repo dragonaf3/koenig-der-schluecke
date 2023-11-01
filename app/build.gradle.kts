b4450d777d20e4850d8d0e8cecc77120d64196da
drago <97@dragonaf.eu>
2023-11-01 03:14:13 +0100

    Kamera Funktionalität in Spiel beitreten hinzugefügt. QR-Funktionalitäten fehlen noch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.4")
     implementation("androidx.navigation:navigation-ui:2.7.4")
+    implementation("com.journeyapps:zxing-android-embedded:4.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
